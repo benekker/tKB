4888e5bd34a55b3779be44f33c7d3fb330e0576d
Ben Ekker <Ben.Ekker@avidityscience.com>
2023-02-09 14:58:43 -0600

    Created navbar component + cleanup

--- tkb/src/App.js
+++ tkb/src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className=''>Hello</h1>
+      <Navbar/>
     </div>
   );
 }
