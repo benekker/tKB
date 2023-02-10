52a8987d8bd762750d569cf87095a35a1ca1be1e
Ben Ekker <Ben.Ekker@avidityscience.com>
2023-02-10 12:26:45 -0600

    Installed dnd + uuid + created basic kanban model

--- tkb/src/App.js
+++ tkb/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Kanban from './components/Kanban';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Kanban/>
     </div>
   );
 }
